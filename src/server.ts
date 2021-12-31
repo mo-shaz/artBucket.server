5189aa455863af27b3d86fa56e88c6d8c31efc3a
mo-shaz <mhmmdshaz98@gmail.com>
2021-12-31 20:24:15 +0530

    session secure = true

--- src/server.ts
+++ src/server.ts
@@ -136,7 +136,7 @@ server.register(cookie)
 server.register(session, {
     cookieName: 'sessionId',
     secret: (process.env.SESSION_SECRET as string),
-    cookie: { secure: false, maxAge: 604800000, httpOnly: true}
+    cookie: { secure: "auto", maxAge: 604800000, httpOnly: true}
 })
 
 // MULTER
